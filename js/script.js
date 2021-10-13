3d9a0ca4877718226fbec1bdd1aac3620c5b8cc2
AzazelDR - Axel Reyes <49660378+AzazelDR@users.noreply.github.com>
2021-10-13 16:30:53 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
+alert("correo: user@mail - contraseña: 123 ");
+
 var attempt = 3; // Variable to count number of attempts.
 // Below function Executes on click of login button.
 function validate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if (username == "user" && password == "123") {
-        alert("Bienvenido " + username);
+    if (username == "user@mail" && password == "123") {
+        alert("Bienvenido");
         window.location = "home.html"; // Redirecting to other page.
         return false;
     } else {
